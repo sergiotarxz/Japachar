3d0477764be49ef6b1a584956314bc4f3cc86491
Sergiotarxz <sergiotarxz@posteo.net>
2025-07-27 20:46:20 +0200

    Adding initial tokenizer.

---
+++ tokenizer/src/main/java/me/sergiotarxz/japachar/tokenizer/Main.java
@@ -0,0 +1,15 @@
+package me.sergiotarxz.japachar.tokenizer;
+
+import java.util.List;
+import com.atilika.kuromoji.ipadic.Token;
+import com.atilika.kuromoji.ipadic.Tokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Tokenizer tokenizer = new Tokenizer() ;
+        List<Token> tokens = tokenizer.tokenize("お寿司が食べたい。");
+        for (Token token : tokens) {
+            System.out.println(token.getSurface() + "\t" + token.getAllFeatures());
+        }
+    }
+}
